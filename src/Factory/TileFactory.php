a4055542258d20331b22447e82c93f63dd5bb2b1
Tom Bury <tom@negocom-atlantique.com>
2026-09-09 16:59:58 +0200

    intégration d'une factory d création

---
+++ src/Factory/TileFactory.php
@@ -0,0 +1,50 @@
+<?php
+
+class TileFactory {
+    public function create(TileType $type, string $name, Square $position, array $options = []): Tile {
+        return match ($type) {
+            TileType::GO => new Go(
+                $name, 
+                $position
+            ),
+            TileType::JAIL => new Jail(
+                $name, 
+                $position
+            ),
+            TileType::FREE_PARKING => new FreeParking(
+                $name, 
+                $position
+            ),
+            TileType::CHANCE => new Chance(
+                $name, 
+                $position
+            ),
+            TileType::COMMUNITY_CHEST => new CommunityChest(
+                $name, 
+                $position
+            ),
+
+            TileType::TAX => new Tax(
+                $name, $position, 
+                $options['amount'] ?? 0 
+            ),
+            TileType::STATION => new Station(
+                $name, $position, 
+                $options['price'] ?? 0
+            ),
+            TileType::COMPANY => new Company(
+                $name, $position, 
+                $options['price'] ?? 0
+            ),
+
+            TileType::PROPERTY => new Property(
+                $name, 
+                $position, 
+                $options['colorGroup'] ?? throw new MonopolyException("Le paramètre 'colorGroup' est requis pour créer une Property."), 
+                $options['price'] ?? 0 ,
+                $options['rent'] ?? 0
+            )
+        };
+    }
+
+}
